3cfa17e2b061b011899c1d6ec0cfea98b36a75f5
Vladimir Smirnov <civil.over@gmail.com>
2025-05-18 22:47:16 +0200

    Add profiles from trex and my own simple generator
    Also fix start script to actually pass useful arguments

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export PYTHONPATH="$(pwd)/interactive:$(pwd)/interactive/trex:$(pwd)/interactive/trex/examples:$(pwd)/interactive/trex/examples/stl"
-
+export TREX_EXT_LIBS="$(pwd)/external_libs"
+export STL_PROFILES_PATH="$(pwd)/profiles"
 echo ${PYTHONPATH}
-python3 -msimple_bench -r 5mpps
+python3 -msimple_bench -t $((300*1000)) -b 10000 -s 50
